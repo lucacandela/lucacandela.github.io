d7419a3cb5c078c7c891f82d7ef7226c90cbd1c5
Luca C <39223795+lucacandela@users.noreply.github.com>
2024-12-05 17:13:49 -0800

    teehee

---
+++ main.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+import './style.css';
+
+
+const diceContainers = document.querySelectorAll('.diceContainer');
+
+//Add click Listener
+diceContainers.forEach((container, index) => {
+  
+  //Hide everything but the first dicebox
+  if (index > 0 ) {
+    container.style.display = 'none';
+  }
+
+  //Handle what happens when clicked
+  container.addEventListener('click', () => {
+    const img = container.querySelector('img');
+    if (img) {
+      //Change img src to just the frame
+      img.src = 'images/DiceFrame.svg';
+
+      //Unhide the next Dicecontainer when clicked
+      if (index < diceContainers.length -1){
+        diceContainers[index+1].style.display = 'block';
+        diceContainers[index+1].parentElement.style.display = 'flex'
+      }
+    }
+  });
+});
